07bb4f0964dbefa8aa779edcbd942eb1e4772b10
Hudson Tadeu <hudinvesting@gmail.com>
2023-10-26 11:15:59 -0300

    Passagem de parâmetros

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/empresa', function () {
+Route::get('/empresa', function() {
     return view('site/empresa');
 });
 
-Route::any('/any', function () {
+Route::any('/any', function() {
     return "Permite todo tipo de acesso http(put, delete, get, post)";
 });
 
-Route:: match(['put', 'delete'], '/match', function () {
+Route::match(['put', 'delete'], '/match', function() {
     return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat?}', function($id, $cat = '') {
+    return "O id do produto é:".$id."<br>"."A categoria é:".$cat;
 });
